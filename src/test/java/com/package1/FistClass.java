f98c6f2d79c9d478619b47fb2f6575211aab61cf
Subbu255 <subramani25597@gmail.com>
2023-08-17 15:26:01 +0530

    new project for gitbash proctice

---
+++ src/test/java/com/package1/FistClass.java
@@ -0,0 +1,13 @@
+package com.package1;
+
+public class FistClass {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("---------hello-----");
+		System.out.println("------good morning -----");
+		
+	}
+
+}
