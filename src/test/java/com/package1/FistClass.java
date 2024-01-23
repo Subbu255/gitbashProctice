f164910ac343f156bcb5ed8c4d218c5c5a9d6b85
Subbu255 <128217060+Subbu255@users.noreply.github.com>
2024-01-23 09:17:55 +0000

    modifications done

--- src/test/java/com/package1/FistClass.java
+++ src/test/java/com/package1/FistClass.java
@@ -3,8 +3,7 @@ package com.package1;
 public class FistClass {
 
 	public static void main(String[] args) {
-
-		
+	
 		System.out.println("---------hello-----");
 		System.out.println("------good morning -----");
 		System.out.println("----good afternoon --");
