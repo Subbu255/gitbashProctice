bde07c7ed0eff623fc86aff02a8d63b03500b8c9
Subbu255 <subramani25597@gmail.com>
2024-03-29 21:38:25 +0530

    new line added

--- src/test/java/com/package1/FistClass.java
+++ src/test/java/com/package1/FistClass.java
@@ -3,13 +3,11 @@ package com.package1;
 public class FistClass {
 
 	public static void main(String[] args) {
-	
 		System.out.println("---------hello-----");
 		System.out.println("------good morning -----");
 		System.out.println("----good afternoon --");
 		System.out.println("---bye ----");
-		system.out.println();
-		
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 	}
 
 }
