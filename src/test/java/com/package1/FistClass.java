7c2f5c7720afe5f0dd51526f0363faaa6f10c5da
Subbu255 <subramani25597@gmail.com>
2024-03-29 21:58:49 +0530

    fixed the bugs

--- src/test/java/com/package1/FistClass.java
+++ src/test/java/com/package1/FistClass.java
@@ -7,10 +7,12 @@ public class FistClass {
 		System.out.println("------good morning -----");
 		System.out.println("----good afternoon --");
 		System.out.println("---bye ----");
-		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 		System.out.println("------okay------");
 		System.out.println("===");
 		System.out.println();
+		System.out.println("..........................................");
+
 	}
 
 }
