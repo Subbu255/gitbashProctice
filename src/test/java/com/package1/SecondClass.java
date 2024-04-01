59fe76b7d4bc64c334536d154a4351a14624b197
Subbu255 <subramani25597@gmail.com>
2024-04-01 11:07:40 +0530

    changes from intellij idea

--- src/test/java/com/package1/SecondClass.java
+++ src/test/java/com/package1/SecondClass.java
@@ -1,2 +1,10 @@
-package com.package1;public class SecondClass {
+package com.package1;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+        System.out.println("This is Second Class");
+    }
+
+
 }
