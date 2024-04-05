23cad1989b8fb4abd83b91a94d3c6759f248e4e5
Subbu255 <subramani25597@gmail.com>
2024-04-05 18:09:57 +0530

    new line added

--- src/test/java/com/package1/SecondClass.java
+++ src/test/java/com/package1/SecondClass.java
@@ -5,6 +5,8 @@ public class SecondClass {
 
     public static void main(String[] args) {
         System.out.println("This is Second Class");
-        System.out.println(.......");
+        System.out.println(".......");
+
+        System.out.println(".............");
     }
 }
